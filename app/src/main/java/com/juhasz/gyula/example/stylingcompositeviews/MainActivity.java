0ed7f424d99b7e46b9c1c64d5f6d42db308d17e7
Gyula Juháasz <gyu999@gmail.com>
2016-04-04 19:03:36 +0200

    Example project created

---
+++ app/src/main/java/com/juhasz/gyula/example/stylingcompositeviews/MainActivity.java
@@ -0,0 +1,16 @@
+package com.juhasz.gyula.example.stylingcompositeviews;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Main activity to test the example View.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+    }
+}
